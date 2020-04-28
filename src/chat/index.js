6d8c85bcb4a404077da647457cf992163aa7d3a9
equidistant <mich.random@pm.me>
2020-04-28 12:35:55 +0200

    handle socket upgrade

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,14 +1,33 @@
 import WebSocket from 'ws'
 import { logStartService } from '../utils'
 
+// const runChat = (server) => {
+//   const wss = new WebSocket.Server({ server: server, path: '/chat' })
+//   logStartService('Chat Server')
+//   wss.on('connection', (ws, req) => {
+//     ws.on('message', (message) => {
+//
+//     })
+//     ws.send('response')
+//   })
+// }
+
 const runChat = (server) => {
-  const wss = new WebSocket.Server({ server: server, path: '/chat' })
+  const wss = new WebSocket.Server({ noServer: true, path: '/chat' })
   logStartService('Chat Server')
   wss.on('connection', (ws, req) => {
     ws.on('message', (message) => {
-      console.log('received: %s', message)
+      console.log(message)
+      ws.send(`received: ${message}`)
     })
-    ws.send('response')
+    ws.send('opened connection')
+  })
+
+  server.on('upgrade', (req, socket, head) => {
+    console.log(req.headers)
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit('connection', ws, req)
+    })
   })
 }
 
